d2e882a676b02e955ec9277d2e0cd76ac262fafd
shubham8patni <shubhampatni88@gmail.com>
2022-08-17 17:48:35 +0530

    1.connected to auroraDB using API requests 2.improved general flow and UI bugs

--- portal/auth.py
+++ portal/auth.py
@@ -1,9 +1,7 @@
-
 from flask import Blueprint, render_template, request, flash, session, redirect, url_for
-from connection import mydb
-from flask_bcrypt import check_password_hash
+from flask_bcrypt import check_password_hash, generate_password_hash
+import requests
 
-mycursor = mydb.cursor()
 
 auth = Blueprint('auth', __name__) # not necessary to name variable same as file name and not necessay to name
 
@@ -18,23 +16,31 @@ def login():
     if request.method == 'POST':
         email = request.form.get("email")
         password = request.form.get("password") # Shubham8@arrzi
-        sql =  f"select * from `users` where `email` = '{email}'"
-        mycursor.execute(sql)
-        result = mycursor.fetchall()
-        if len(result)==0:
+
+        # make API call to get workers list
+        api_url = "https://8m2febviee.execute-api.ap-south-1.amazonaws.com/Dev/flask"
+
+        query = {'query': f"select * from `portal_users` where `email` = '{email}'"}
+        headers = {'Content-type': 'application/json', 'authToken':'282e23176845652581e80b39776ad09b8e59652b69106509053efd2f8c53d821'}
+        response = requests.post(api_url, json=query, headers=headers)
+        response = response.json()
+        # print("111111111111111111111111111",response)
+        
+        if len(response['body']['records'])==0:
             flash('Not a Registered Email! Please contact Admin!', category='error')
             return redirect(url_for('auth.login'))
-        print(result[0][5], password)
-        if check_password_hash(result[0][5], password)==True:
-            session["email"] = result[0][1]
-            session["name"] = result[0][2]
-            session["phone_number"] = result[0][3]
-            session["user_level"] = 1
+        # print(response['body']['records'][5]['stringValue'], password)
+        if check_password_hash(response['body']['records'][0][5]['stringValue'], password)==True:
+            session["email"] = response['body']['records'][0][1]['stringValue']
+            session["name"] = response['body']['records'][0][2]['stringValue']
+            session["phone_number"] = response['body']['records'][0][3]['stringValue']
+            session["user_level"] = response['body']['records'][0][4]['longValue']
             flash('Login Successful!', category='success')
             return redirect(url_for('views.index', session = session))
         else:
             flash('Wrong Password! try again!', category='error')
             return redirect(url_for('auth.login'))
+
     else:
         return render_template("login.html")
 
@@ -49,6 +55,25 @@ def logout(status):
     return redirect(url_for("auth.login"))
 
 
-@auth.route('/register', methods = ['GET','POST'])
+@auth.route('/register', methods=["POST", "GET"])
 def register():
-    return "Register"
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+        name = request.form.get("name")
+        phone_number = request.form.get("phone_number")
+        user_level = int(1)
+        pw_hash = generate_password_hash(password).decode('utf8')
+        # print("111111111111111111111111111111111111111111111111111111111111111111111", pw_hash,)
+        # make API call to get workers list
+        api_url = "https://8m2febviee.execute-api.ap-south-1.amazonaws.com/Dev/flask"
+
+        query = {'query': f'INSERT INTO `portal_users`(`email`, `full_name`, `phone_number`, `user_level`, `password`) VALUES ("{email}","{name}",{phone_number},{user_level},"{pw_hash}") '}
+        headers = {'Content-type': 'application/json', 'authToken':'282e23176845652581e80b39776ad09b8e59652b69106509053efd2f8c53d821'}
+        response = requests.post(api_url, json=query, headers=headers)
+        response = response.json()
+        print(response)
+        flash('Admin account created successfully', category='success')
+        return redirect(url_for('auth.login'))
+    else:
+        return render_template("register.html")
--- portal/views.py
+++ portal/views.py
@@ -1,9 +1,8 @@
+
 from flask import Blueprint, render_template, request, session, redirect, url_for, flash
 import json
-from connection import mydb
+import requests
 
-
-mycursor = mydb.cursor()
 
 views = Blueprint('views', __name__) # not necessary to name variable same as file name and not necessay to name
 
@@ -24,12 +23,49 @@ def index():
 
 
 
-@views.route('/view_assignment', methods = ['POST','GET'])
+@views.route('/view_assignment', methods = ['GET'])
 def view_assignments():
     # return redirect(url_for('success',email = user))
     if not session.get("email") or session.get('user_level') != 1:       
         return redirect(url_for('auth.logout', status = 'error'))
-    if request.method == "POST":
+
+    ans = []
+    # make API call to get workers list
+    api_url = "https://8m2febviee.execute-api.ap-south-1.amazonaws.com/Dev/flask"
+
+    query = {'query': f"select * from assignments where worker_assigned < worker_needed and status = 'active'"}
+    headers = {'Content-type': 'application/json', 'authToken':'282e23176845652581e80b39776ad09b8e59652b69106509053efd2f8c53d821'}
+    response = requests.post(api_url, json=query, headers=headers)
+    response = response.json()
+
+    for i in response['body']['records']:
+        fin_ans = {
+                    "assignment_id" : i[0]['longValue'],
+                    "contractor_id": i[1]['longValue'],
+                    "create_date": i[2]['stringValue'],
+                    "start_date": i[3]['stringValue'],
+                    "end_date" : i[4]['stringValue'],
+                    "worker_needed": i[5]['longValue'],
+                    "wage": i[6]['longValue'],
+                    "skill_needed": i[7]['longValue'],
+                    "city" : i[8]['longValue'],
+                    "worker_assigned": i[9]['longValue'],
+                    "status": i[10]['stringValue']          
+                }
+        ans.append(fin_ans)
+    return render_template("view_assignment.html", ans=ans)
+
+
+
+@views.route('/workers_list', methods=[ "POST"])
+def workers_list():
+    if not session.get("email") or session.get('user_level') != 1:
+        return redirect(url_for('auth.logout', status = 'error'))
+    if request.method == 'POST':
+        
+        # initialize list
+        data1 = []
+        # get assignment data from form
         assignment_id = request.form['assignment_id']
         contractor_id = request.form["contractor_id"]
         create_date = request.form["create_date"]
@@ -42,74 +78,51 @@ def view_assignments():
         worker_assigned = request.form["worker_assigned"]
         status = request.form[ "status"]  
 
-        create = {
-                    "assignment_id" : assignment_id,
-                    "contractor_id": contractor_id ,
-                    "create_date": create_date ,
-                    "start_date": start_date,
-                    "end_date" : end_date,
-                    "worker_needed": worker_needed,
-                    "wage": wage,
-                    "skill_needed": skill_needed,
-                    "city" : city,
-                    "worker_assigned": worker_assigned,
-                    "status": status          
-                }
-        data=json.dumps(create)
-        
-        return redirect(url_for('views.workers_list', data = data))
-    else:
-        ans = []
-        sql =  "select * from assignments where worker_assigned < worker_needed"
-        mycursor.execute(sql)
-        result = mycursor.fetchall()
-
-        for i in result:
-            fin_ans = {
-                        "assignment_id" : i[0],
-                        "contractor_id": i[1],
-                        "create_date": i[2],
-                        "start_date": i[3],
-                        "end_date" : i[4],
-                        "worker_needed": i[5],
-                        "wage": i[6],
-                        "skill_needed": i[7],
-                        "city" : i[8],
-                        "worker_assigned": i[9],
-                        "status": i[10]          
+        # convert assignment details to json format to parse
+        data = {
+                        "assignment_id" : assignment_id,
+                        "contractor_id": contractor_id ,
+                        "create_date": create_date ,
+                        "start_date": start_date,
+                        "end_date" : end_date,
+                        "worker_needed": worker_needed,
+                        "wage": wage,
+                        "skill_needed": skill_needed,
+                        "city" : city,
+                        "worker_assigned": worker_assigned,
+                        "status": status          
                     }
-            ans.append(fin_ans)
-        return render_template("view_assignment.html", ans=ans)
+        data1.append(data)
 
-
-
-@views.route('/workers_list/<data>', methods=[ "GET"])
-def workers_list(data):
-    if not session.get("email") or session.get('user_level') != 1:
-        return redirect(url_for('auth.logout', status = 'error'))
-    data = json.loads(data)
-    data1 = []
-    data1.append(data)
-    
-    sql =  "select * from workers where city = '"+ data1[0]['city'] +"'" # AND skill_needed = '"+ data1[0]['skill_needed'] +"'"
-    mycursor.execute(sql)
-    result = mycursor.fetchall()
-    fin_ans = []        
-    for i in result:    
-        ans = {
-            "worker_id" : i[0],
-            "phone_number": i[1],
-            "aadhar_number": i[2],
-            "dob": i[3],
-            "city" : i[4],
-            "full_name": i[5],
-            "start_date": i[6],
-            "notification_token": i[7],
-            "skill" : i[8],
-                    
-        }
-        fin_ans.append(ans)
-    return render_template("worker_list.html", data=data1, data2 = fin_ans)
+        # make API call to get workers list
+        api_url = "https://8m2febviee.execute-api.ap-south-1.amazonaws.com/Dev/flask"
+        city = data1[0]['city']
+        query = {'query': f"select * from workers where city =  '{city}' "} # AND skill_needed = '"+ data1[0]['skill_needed'] +"'"
+        headers = {'Content-type': 'application/json', 'authToken':'282e23176845652581e80b39776ad09b8e59652b69106509053efd2f8c53d821'}
+        response = requests.post(api_url, json=query, headers=headers)
+        response = response.json()
+        # print("11111111111111111111111111111111111111111111111111111111111111111111111111", response['body']['records'])
+        
+        fin_ans = []
+        for i in response['body']['records']:    
+            if 'isNull' in i[2]:
+                aadhar_number="None"
+            else:
+                aadhar_number="i[2]['stringValue']"
+            ans = {
+                "worker_id" : i[0]['longValue'],
+                "phone_number": i[1]['stringValue'],
+                "aadhar_number": aadhar_number,
+                "dob": i[3]['stringValue'],
+                "city" : i[4]['longValue'],
+                "full_name": i[5]['stringValue'],
+                "start_date": i[6]['stringValue'],
+                "notification_token": i[7]['stringValue'],
+                "skill" : i[8]['longValue'],
+                        
+            }
+            fin_ans.append(ans)
+        return render_template("worker_list.html", data=data1, data2 = fin_ans)
 
 
 @views.route('/assign_worker', methods=["POST"])
@@ -118,17 +131,49 @@ def assign_worker():
         return redirect(url_for('auth.logout', status = 'error'))
     if request.method == 'POST':
         assignment_id = request.form.get("assignment_id")
+        assigned_worker = request.form.get("worker_assigned")
         worker_id = request.form.getlist("worker_id_check")
+
         for i in range(len(worker_id)):
             worker_id[i] = int(worker_id[i])
         if len(worker_id) == 0:
             flash('No workers assigned', category='success')
             return redirect(url_for('views.index'))
         else:
-            flash(f'{len(worker_id)} workers assigned', category='success')
+            
             for i in worker_id:
-                sql =  "INSERT INTO `assignment_status` (`assignment_id`,`worker_id`,`status`) VALUES('{assignment_id}', '{i}', 'accepted')"
-                return redirect(url_for('views.index'))
+                # make API call to check if the worker already has an entry for this assignment
+                api_url = "https://8m2febviee.execute-api.ap-south-1.amazonaws.com/Dev/flask"
+                query = {'query': f"select * from `assignment_status` where assignment_id =  '{assignment_id}' and worker_id = '{i}' "}
+                headers = {'Content-type': 'application/json', 'authToken':'282e23176845652581e80b39776ad09b8e59652b69106509053efd2f8c53d821'}
+                response = requests.post(api_url, json=query, headers=headers)
+                response = response.json()
+                # print("11111111111111111111111111111111111111111111111111111111111111111111111111", response['body']['records'])
+                if len(response['body']['records']) == 0:
+                    # make API call to make new entry in assignment_status table
+                    api_url = "https://8m2febviee.execute-api.ap-south-1.amazonaws.com/Dev/flask"
+                    query = {'query': f"INSERT INTO `assignment_status` (`assignment_id`,`worker_id`,`status`) VALUES('{assignment_id}', '{i}', 'accepted')"}
+                    headers = {'Content-type': 'application/json', 'authToken':'282e23176845652581e80b39776ad09b8e59652b69106509053efd2f8c53d821'}
+                    response = requests.post(api_url, json=query, headers=headers)
+                    # response = response.json()
+                    
+                elif len(response['body']['records']) > 0:
+                    # make API call to update entry in assignment_status table
+                    api_url = "https://8m2febviee.execute-api.ap-south-1.amazonaws.com/Dev/flask"
+                    query = {'query': f"UPDATE `assignment_status` SET status = 'accepted' where assignment_id =  '{assignment_id}' and worker_id = '{i}' "} 
+                    headers = {'Content-type': 'application/json', 'authToken':'282e23176845652581e80b39776ad09b8e59652b69106509053efd2f8c53d821'}
+                    response = requests.post(api_url, json=query, headers=headers)
+
+                # increment the value of asssigned workers
+                assigned_worker = int(assigned_worker) + 1
+                # make API call to inccrement assigned_worker in assignment table
+                api_url = "https://8m2febviee.execute-api.ap-south-1.amazonaws.com/Dev/flask"
+                query = {'query': f"UPDATE `assignments` SET worker_assigned = {assigned_worker} WHERE assignment_id = {assignment_id} "}
+                headers = {'Content-type': 'application/json', 'authToken':'282e23176845652581e80b39776ad09b8e59652b69106509053efd2f8c53d821'}
+                response = requests.post(api_url, json=query, headers=headers)
+                print(response.json())
+            flash(f'{len(worker_id)} workers assigned', category='success')
+            return redirect(url_for('views.index'))
         
     else:
         return redirect(url_for('auth.logout' , status = 'error'))
